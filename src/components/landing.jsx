964171bce5d862ca59f3ca0ea67f16b128e88e2c
Rohith Prakash <rohithprakash99@gmail.com>
2020-02-27 14:08:10 +0530

    bug fix

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 import Row from "react-bootstrap/Row";
 import Typography from '@material-ui/core/Typography';
 
--- src/components/question.jsx
+++ src/components/question.jsx
@@ -3,8 +3,10 @@ import React, { Component } from 'react'
 export default class Question extends Component {
     render() {
         return (
-            <div>
+            <div className="min-vh-100">
+                <div className="m">
 
+                </div>
             </div>
         )
     }

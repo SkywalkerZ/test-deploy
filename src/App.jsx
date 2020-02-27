09e9f5e5e674b5b99fc17de246236ec9d73e82dc
Rohith Prakash <rohithprakash99@gmail.com>
2020-02-27 13:13:23 +0530

    routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Landing from "./components/landing";
+import Option from "./components/option";
+import Question from "./components/question";
+import NotFound from "./components/notFound";
 
 export default class App extends Component {
     render() {
         return (
-            <div>
-
-            </div>
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={Landing} />
+                    <Route path="/question" component={Question} />
+                    <Route path="/option" component={Option} />
+                    <Route component={NotFound} />
+                </Switch>
+            </Router>
         )
     }
 }
---
+++ src/components/notFound.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+import './styles/notFound.css';
+
+export default class NotFound extends Component {
+    render() {
+        return (
+            <div>
+                <div id="notfound">
+                    <div class="notfound">
+                        <div class="notfound-404">
+                            <h1>Oops!</h1>
+                            <h2>404 - The Page can't be found</h2>
+                        </div>
+                        <a href="/">Go TO Homepage</a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}

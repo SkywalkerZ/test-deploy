7d8c1f3adff854f8a02fe431186b96c921196f6d
SkywalkerZ <abraarshk98@gmail.com>
2020-02-27 16:16:18 +0530

    landing page theming #2

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -4,6 +4,7 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Row from "react-bootstrap/Row";
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../assets/style/main.css";
@@ -33,84 +34,82 @@ export default class Landing extends Component {
                         <h1 className="p-3 display-2"><u>National Science Day</u></h1>
                         <h1 className="m-2 display-4"><u>Science Quiz</u></h1>
                     </div>
-                    <Row id="row1">
-                        <div className="col-12 col-xs-12 col-sm-12 col-md-6 col-lg-6" id="batch1a">
+                    <Row id="row1-main">
+                        <div className="col-6 col-xs-6 col-sm-6 col-md-2 col-lg-2 mr-1" id="batch1a">
                             <Card className={useStyles.root}>
                                 <CardContent>
                                     <Typography className={useStyles.title} color="textSecondary" gutterBottom>
                                         Time
                   </Typography>
                                     <Typography variant="h5" component="h2">
-                                        Batch 1A
+                                        <Button size="small">Batch 1A</Button>
                   </Typography>
 
                                 </CardContent>
                             </Card>
                         </div>
-                        <div className="col-12 col-xs-12 col-sm-12 col-md-6 col-lg-6" id="batch1b">
+                        <div className="col-6 col-xs-6 col-sm-6 col-md-2 col-lg-2" id="batch1b">
                             <Card className={useStyles.root}>
                                 <CardContent>
                                     <Typography className={useStyles.title} color="textSecondary" gutterBottom>
                                         Time
                   </Typography>
                                     <Typography variant="h5" component="h2">
-                                        Batch 1B
+                                    <Button size="small">Batch 1B</Button>
                   </Typography>
 
                                 </CardContent>
                             </Card>
                         </div>
-                    </Row>
-                    <Row id="row2">
-                        <div className="col-12 col-xs-12 col-sm-12 col-md-6 col-lg-6" id="batch2">
+                        <div className="col-6 col-xs-6 col-sm-6 col-md-2 col-lg-2" id="batch2">
                             <Card className={useStyles.root}>
                                 <CardContent>
                                     <Typography className={useStyles.title} color="textSecondary" gutterBottom>
                                         Time
                   </Typography>
                                     <Typography variant="h5" component="h2">
-                                        Batch 2
-                  </Typography>
-
-                                </CardContent>
-                            </Card>
-                        </div>
-                        <div className="col-12 col-xs-12 col-sm-12 col-md-6 col-lg-6" id="batch3">
-                            <Card className={useStyles.root}>
-                                <CardContent>
-                                    <Typography className={useStyles.title} color="textSecondary" gutterBottom>
-                                        Time
-                  </Typography>
-                                    <Typography variant="h5" component="h2">
-                                        Batch 3
+                                    <Button size="small"> Batch 2</Button>
                   </Typography>
 
                                 </CardContent>
                             </Card>
                         </div>
                     </Row>
-                    <Row id="row3">
-                        <div className="col-12 col-xs-12 col-sm-12 col-md-6 col-lg-6" id="batch4">
+                    <Row id="row2-main">
+                    <div className="col-6 col-xs-6 col-sm-6 col-md-2 col-lg-2" id="batch3">
+                    <Card className={useStyles.root}>
+                        <CardContent>
+                            <Typography className={useStyles.title} color="textSecondary" gutterBottom>
+                                Time
+          </Typography>
+                            <Typography variant="h5" component="h2">
+                            <Button size="small"> Batch 3</Button>
+          </Typography>
+
+                        </CardContent>
+                    </Card>
+                </div>
+                        <div className="col-6 col-xs-6 col-sm-6 col-md-2 col-lg-2" id="batch4">
                             <Card className={useStyles.root}>
                                 <CardContent>
                                     <Typography className={useStyles.title} color="textSecondary" gutterBottom>
                                         Time
                   </Typography>
                                     <Typography variant="h5" component="h2">
-                                        Batch 4
+                                    <Button size="small">Batch 4</Button>
                   </Typography>
 
                                 </CardContent>
                             </Card>
                         </div>
-                        <div className="col-12 col-xs-12 col-sm-12 col-md-6 col-lg-6" id="batch5">
+                        <div className="col-6 col-xs-6 col-sm-6 col-md-2 col-lg-2" id="batch5">
                             <Card className={useStyles.root}>
                                 <CardContent>
                                     <Typography className={useStyles.title} color="textSecondary" gutterBottom>
                                         Time
                   </Typography>
                                     <Typography variant="h5" component="h2">
-                                        Batch 5
+                                    <Button size="small"> Batch 5</Button>
                   </Typography>
 
                                 </CardContent>

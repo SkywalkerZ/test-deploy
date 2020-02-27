9f9c29fd0f6c30428b6d56ff27bbd5a682dff559
Rohith Prakash <rohithprakash99@gmail.com>
2020-02-27 16:27:02 +0530

    timer made

--- src/components/question.jsx
+++ src/components/question.jsx
@@ -2,7 +2,30 @@ import React, { Component } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default class Question extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            seconds: 30,
+            message: ""
+        }
+    }
+
+    componentDidMount() {
+        this.myInterval = setInterval(() => {
+            if (this.state.seconds > 0) {
+                this.setState(({ seconds }) => ({
+                    seconds: seconds - 1
+                }))
+            } if (this.state.seconds === 0) {
+                this.setState({ message: "Time's up" })
+                clearInterval(this.myInterval)
+            }
+        }, 1000)
+    }
+
     render() {
+        const question = this.props.question
         return (
             <div className="min-vh-100">
                 <div className="m-4">
@@ -11,9 +34,10 @@ export default class Question extends Component {
                     </div>
                     <div className="p-4 text-center">
                         <div>
-                            <h1 className="displau-4 p-3">00:30</h1>
+                            <h1 className="display-3">{this.state.seconds}</h1>
+                            <h1 className="display-3 mt-2">{this.state.message}</h1>
                         </div>
-                        <h1 className="display-3">Question</h1>
+                        <h1 className="display-2">{question}</h1>
                     </div>
                 </div>
             </div>

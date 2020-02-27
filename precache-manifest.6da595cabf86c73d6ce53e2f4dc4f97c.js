cd8111d357730d470ea6abe2ef095769ea879366
SkywalkerZ <abraarshk98@gmail.com>
2020-02-27 19:40:37 +0530

    Updates

---
+++ precache-manifest.6da595cabf86c73d6ce53e2f4dc4f97c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6b3a3c61d2df3c9eb085909b1bec7a2",
+    "url": "/test-deploy/index.html"
+  },
+  {
+    "revision": "fb64cd65a5a797e66e79",
+    "url": "/test-deploy/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "7bb2d0d9d5c32ccb9668",
+    "url": "/test-deploy/static/css/main.89a15458.chunk.css"
+  },
+  {
+    "revision": "fb64cd65a5a797e66e79",
+    "url": "/test-deploy/static/js/2.c66b538f.chunk.js"
+  },
+  {
+    "revision": "c099810c3c102db7a44755001529c804",
+    "url": "/test-deploy/static/js/2.c66b538f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bb2d0d9d5c32ccb9668",
+    "url": "/test-deploy/static/js/main.2832d058.chunk.js"
+  },
+  {
+    "revision": "4932602684da29a86112",
+    "url": "/test-deploy/static/js/runtime-main.3cf4b34a.js"
+  }
+]);
